360e395bc269a95dc5049955d9317a80c2af3d2b
bob234566 <132169561+bob234566@users.noreply.github.com>
2025-07-19 21:54:03 -0500

    Add files via upload

---
+++ tim/game_logic.js
@@ -0,0 +1,5 @@
+window.onload = () => {
+  document.getElementById("loading-screen").style.display = "none";
+  document.body.style.background = "#fff0f5";
+  console.log("Game started: HitCandy VR");
+};
